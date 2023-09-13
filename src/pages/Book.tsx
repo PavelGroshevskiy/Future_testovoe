e7424acc68788e29a93eaccd7f157591c821d542
Pavel Groshevskiy <pavel.groshevskiy@gmail.com>
2023-09-13 17:36:16 +0700

    refactor fetching one book from RTKQ

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,31 +1,16 @@
-import React from "react";
 import { OneBookCard, Skeleton } from "../modules/ShowResult";
 import { useParams } from "react-router-dom";
-import axios from "axios";
-import { API_KEY } from "../modules/ShowResult/constants/apiKey";
+
+import { useSerarchOneBookQuery } from "../modules/ShowResult/store/fetchBooks";
 
 const Book = () => {
-	const [oneBook, setOneBook] = React.useState();
 	const { id } = useParams();
-
-	React.useEffect(() => {
-		const fetchOneBook = async () => {
-			try {
-				const { data } = await axios.get(
-					`https://www.googleapis.com/books/v1/volumes/${id}?key=${API_KEY}`
-				);
-				setOneBook(data.volumeInfo);
-			} catch (e) {
-				console.log("Ошибка при получении одной книги");
-			}
-		};
-		fetchOneBook();
-	}, []);
+	const { data } = useSerarchOneBookQuery(id);
 
 	return (
 		<div>
 			<h1>Book</h1>
-			{oneBook ? <OneBookCard bookObj={oneBook} /> : <Skeleton />}
+			{data ? <OneBookCard bookObj={data} /> : <Skeleton />}
 		</div>
 	);
 };
